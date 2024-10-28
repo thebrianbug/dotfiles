6f9a4a234e962a74cd47bd827bfc542d160e42ac
Brian Mcilwain <brianmcilwain@Brians-MacBook-Pro.local>
2024-10-28 15:42:34 -0300

    Hack to add Applications to Spotlight

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -9,13 +9,14 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
          vim
          curl
          gitAndTools.gitFull
+         mkalias
          obsidian
       ];
 
@@ -67,6 +68,27 @@
            { HIDKeyboardModifierMappingSrc = 30064771296; HIDKeyboardModifierMappingDst = 30064771129; }
          ];
       };
+
+      # Add Applications to Spotlight hack
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
 
     };
   in
