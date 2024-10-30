7fa1093572af7cf6ca7187a01d973e9f7cb9ae0a
Brian Mcilwain <brianmcilwain@Brians-MacBook-Pro.local>
2024-10-30 06:52:25 -0300

    Installed home-manager

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -6,9 +6,13 @@
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
   let
     configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -139,11 +143,31 @@
       modules = [ 
         configuration
         nix-homebrew.darwinModules.nix-homebrew
+        home-manager.darwinModules.home-manager
         {
           nix-homebrew = {
             enable = true;
             # User owning Homebrew prefix
             user = "brianmcilwain";
+          };
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          
+          users.users.brianmcilwain = {
+            name = "brianmcilwain";
+            home = "/Users/brianmcilwain";
+          };
+          
+          # Home Manager configuration for user
+          home-manager.users.brianmcilwain = { pkgs, ... }: {
+            home.stateVersion = "24.05";
+
+            home.packages = [
+              pkgs.atool
+              pkgs.httpie
+            ];
+            programs.bash.enable = true;
           };
         }
       ];
