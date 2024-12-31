7e8db0b6504294840133b80ab1e0a2260a41a69c
Brian Bug <thebrianbug@gmail.com>
2024-12-30 18:40:57 -0600

    Added coding tools

--- nix-linux/home.nix
+++ nix-linux/home.nix
@@ -27,34 +27,44 @@
     pkgs.obsidian
     pkgs.discord
     pkgs.rclone
+    pkgs.zoom-us
+    pkgs.google-chrome
+    pkgs.vscodium
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # Dev
+    pkgs.fzf
+    pkgs.python39
+    pkgs.nodejs
+    pkgs.nodePackages.live-server
+    pkgs.nodePackages.nodemon
+    pkgs.nodePackages.prettier
+    pkgs.nodePackages.npm
+    pkgs.nodePackages.typescript
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+    ];
+  };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    plugins = [
+      {
+        name = "vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      }
+    ];
+  };
+
+  home.file = {
   };
 
   # Home Manager can also manage your environment variables through
@@ -74,7 +84,7 @@
   #  /etc/profiles/per-user/brianbug/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Enable X server services
