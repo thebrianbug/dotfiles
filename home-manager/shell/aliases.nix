feb871cfd18217f830600c50d3397407d384adce
Brian Bug <thebrianbug@gmail.com>
2025-03-06 13:07:47 -0600

    feat(aliases): add gt alias for decorated git tree with last 10 commits

--- home-manager/shell/aliases.nix
+++ home-manager/shell/aliases.nix
@@ -8,6 +8,7 @@
       gs = "git status";
       gp = "git pull";
       gg = "git push";
+      gt = "git log -n 10 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
   };
 }
