628a9d909b7511f854e45c6eaaef8161b72765be
Brian Bug <thebrianbug@gmail.com>
2025-03-06 13:44:19 -0600

    Remove ghostty

--- home-manager/programs/terminal.nix
+++ home-manager/programs/terminal.nix
@@ -3,15 +3,4 @@
 {
   programs.bash.enable = true;
   programs.home-manager.enable = true;
-
-  # Modern, fast terminal emulator with native UI
-  programs.ghostty = {
-    enable = true;
-    settings = {
-      font-family = "JetBrainsMono Nerd Font";
-      font-size = "12";
-      background-opacity = "0.95";
-    };
-  };
-
 }
