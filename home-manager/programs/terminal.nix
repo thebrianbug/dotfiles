d4d0cb7693ad72b8b1c2572a9c6e0bcc075fd93d
Brian Bug <thebrianbug@gmail.com>
2025-03-06 12:51:50 -0600

    fix: update GNOME Terminal configuration to use proper home-manager options

--- home-manager/programs/terminal.nix
+++ home-manager/programs/terminal.nix
@@ -7,30 +7,30 @@
   # Enable GNOME Terminal
   programs.gnome-terminal = {
     enable = true;
+    profile = {
+      "b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+        default = true;
+        visibleName = "Default";
+        showMenubar = false;
+        audibleBell = false;
+        colors = {
+          backgroundColor = "rgb(23, 20, 33)";
+          foregroundColor = "rgb(208, 207, 204)";
+          useThemeColors = false;
+        };
+        font = {
+          useSystemFont = true;
+        };
+      };
+    };
   };
 
-  # GNOME Terminal configuration
+  # GNOME Terminal keyboard shortcut
   dconf.settings = {
-    # Terminal keyboard shortcut
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control><Alt>t";
       command = "gnome-terminal";
       name = "Launch Terminal";
-    };
-
-    # Terminal preferences
-    "org/gnome/terminal/legacy" = {
-      default-show-menubar = false;
-      theme-variant = "dark";
-    };
-
-    # Default profile settings
-    "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
-      audible-bell = false;
-      background-color = "rgb(23, 20, 33)";
-      foreground-color = "rgb(208, 207, 204)";
-      use-system-font = true;
-      use-theme-colors = false;
     };
   };
 }
