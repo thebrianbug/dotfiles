2290c7c46411a7df6e8619fde83ee91759b978ec
Brian Bug <thebrianbug@gmail.com>
2025-03-06 12:56:28 -0600

    fix: use dconf settings for GNOME Terminal configuration

--- home-manager/programs/terminal.nix
+++ home-manager/programs/terminal.nix
@@ -7,22 +7,27 @@
   # Enable GNOME Terminal
   programs.gnome-terminal = {
     enable = true;
-    settings = {
-      "b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
-        default = true;
-        name = "Default";
-        showMenubar = false;
-        audibleBell = false;
-        backgroundColor = "rgb(23, 20, 33)";
-        foregroundColor = "rgb(208, 207, 204)";
-        useThemeColors = false;
-        useSystemFont = true;
-      };
-    };
   };
 
-  # GNOME Terminal keyboard shortcut
+  # GNOME Terminal configuration
   dconf.settings = {
+    "org/gnome/terminal/legacy" = {
+      default-show-menubar = false;
+      theme-variant = "dark";
+    };
+
+    "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+      audible-bell = false;
+      background-color = "rgb(23, 20, 33)";
+      foreground-color = "rgb(208, 207, 204)";
+      use-system-font = true;
+      use-theme-colors = false;
+      visible-name = "Default";
+      default-size-columns = 120;
+      default-size-rows = 35;
+    };
+
+    # Terminal keyboard shortcut
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Control><Alt>t";
       command = "gnome-terminal";
