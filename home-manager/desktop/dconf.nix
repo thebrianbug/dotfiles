0ca56499c847f07ada3ef944fd19ea45d1fea398
Brian Bug <thebrianbug@gmail.com>
2025-03-06 13:00:09 -0600

    refactor: move terminal dconf settings back to dconf.nix

--- home-manager/desktop/dconf.nix
+++ home-manager/desktop/dconf.nix
@@ -92,5 +92,35 @@
     "org/gnome/desktop/session" = {
       idle-delay = "uint32 0";
     };
+
+    # Terminal configuration
+    "org/gnome/terminal/legacy" = {
+      default-show-menubar = false;
+      theme-variant = "dark";
+    };
+
+    "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+      audible-bell = false;
+      background-color = "rgb(23, 20, 33)";
+      foreground-color = "rgb(208, 207, 204)";
+      use-system-font = true;
+      use-theme-colors = false;
+      visible-name = "Default";
+      default-size-columns = 120;
+      default-size-rows = 35;
+    };
+
+    # Terminal keyboard shortcut
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Control><Alt>t";
+      command = "gnome-terminal";
+      name = "Launch Terminal";
+    };
   };
 }
--- home-manager/programs/terminal.nix
+++ home-manager/programs/terminal.nix
@@ -8,36 +8,4 @@
   programs.gnome-terminal = {
     enable = true;
   };
-
-  # GNOME Terminal configuration
-  dconf.settings = {
-    "org/gnome/terminal/legacy" = {
-      default-show-menubar = false;
-      theme-variant = "dark";
-    };
-
-    "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
-      audible-bell = false;
-      background-color = "rgb(23, 20, 33)";
-      foreground-color = "rgb(208, 207, 204)";
-      use-system-font = true;
-      use-theme-colors = false;
-      visible-name = "Default";
-      default-size-columns = 120;
-      default-size-rows = 35;
-    };
-
-    # Terminal keyboard shortcut
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-      ];
-    };
-
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-      binding = "<Control><Alt>t";
-      command = "gnome-terminal";
-      name = "Launch Terminal";
-    };
-  };
 }
