6a5297e52adc29122ae955ab18c6044af00e1c20
Brian Bug <thebrianbug@gmail.com>
2025-06-21 21:38:38 -0500

    Fix options rename

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -56,8 +56,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable Boxes tooling for VMs
   services.spice-vdagentd.enable = true;
