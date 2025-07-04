0b64f4023731e8fa033e7840f29247d281de6d40
Brian <thebrianbug@gmail.com>
2025-07-04 23:18:42 +0000

    Added timezone

--- hosts/asus-linux/configuration.nix
+++ hosts/asus-linux/configuration.nix
@@ -23,7 +23,7 @@
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "America/Chicago";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
