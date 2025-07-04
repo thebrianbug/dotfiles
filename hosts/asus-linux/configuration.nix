4134ecc621f6820dfd35bf6530e927b8dddd239e
Brian <thebrianbug@gmail.com>
2025-07-04 23:36:04 +0000

    Added flakes, internationalization

--- hosts/asus-linux/configuration.nix
+++ hosts/asus-linux/configuration.nix
@@ -120,6 +120,13 @@
     ];
   };
 
+
+  # Enable flakes
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   # Networking configuration
   networking.networkmanager.enable = true;
 
@@ -130,6 +137,21 @@
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
