0387ea471ba24964b0851672ec28ea16fb44f0b3
Brian Bug <thebrianbug@gmail.com>
2025-07-05 13:28:41 -0500

    Trim comments

--- hosts/asus-linux/configuration.nix
+++ hosts/asus-linux/configuration.nix
@@ -16,11 +16,6 @@
   ];
 
   boot = {
-    # EFI Partition Management: Limit bootloader generations for small EFI partitions
-    # If your EFI partition is small (e.g., 260 MiB), you might be limited to
-    # keeping only one NixOS generation to avoid running out of space.
-    # This limits your rollback capabilities directly from the bootloader.
-    # For more generations, consider expanding your EFI partition.
     loader = {
       grub = {
         enable = true;
@@ -137,9 +132,10 @@
       prime = {
         offload.enable = true;
         sync.enable = false;
+
         # PCI bus IDs for hybrid graphics
-        amdgpuBusId = "PCI:65:00:0"; # AMD GPU at 65:00.0
-        nvidiaBusId = "PCI:64:00:0"; # NVIDIA GPU at 64:00.0
+        amdgpuBusId = "PCI:65:00:0";
+        nvidiaBusId = "PCI:64:00:0";
       };
       open = false; # Prefer propritary driver
     };
@@ -178,11 +174,6 @@
 
   # Networking configuration
   networking.networkmanager.enable = true;
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
@@ -227,15 +218,7 @@
     ];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # Configure keymap in X11 and Wayland
-  # services.xserver.xkb.layout = "us";
-  services.xserver.xkb.options = "ctrl:swapcaps"; # Swap Caps Lock and Control
-
-  # Ensure the same keymap is used in console
-  console.useXkbConfig = true;
 
   # Swap Caps Lock and Control in GNOME
   environment.etc."gsettings/gnome-desktop-input-sources".text = ''
@@ -243,79 +226,7 @@
     xkb-options=['ctrl:swapcaps']
   '';
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services.printing.enable = true;
 
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05"; # DO NOT CHANGE after install, see `man configuration.nix`
 }
