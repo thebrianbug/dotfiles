ed0cb1ec1d1d941f3297385e9fe299d6713d6029
Brian Bug <thebrianbug@gmail.com>
2025-07-05 12:14:23 -0500

    Swap caps lock and control

--- hosts/asus-linux/configuration.nix
+++ hosts/asus-linux/configuration.nix
@@ -204,7 +204,7 @@
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.options = "ctrl:swapcaps";  # Swap Caps Lock and Control
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
