c28f666b7e27ac3d85d42a4efc9d176746c91f5c
Brian Bug <thebrianbug@gmail.com>
2025-07-05 13:31:19 -0500

    Simplify internationalization

--- hosts/asus-linux/configuration.nix
+++ hosts/asus-linux/configuration.nix
@@ -172,28 +172,21 @@
     "flakes"
   ];
 
-  # Networking configuration
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    useXkbConfig = true; # use xkb.options in tty.
+  };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.brianbug = {
     isNormalUser = true;
     description = "Brian Bug";
