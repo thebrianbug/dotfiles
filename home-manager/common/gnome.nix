6610f78701ab142c186aab248deff0e98805379e
Brian Bug <thebrianbug@gmail.com>
2025-07-17 09:24:30 -0500

    Pinned MuseScore

--- home-manager/common/gnome.nix
+++ home-manager/common/gnome.nix
@@ -80,6 +80,7 @@ in
         "vesktop.desktop"
         "org.keepassxc.KeePassXC.desktop"
         "windsurf.desktop"
+        "musescore.desktop"
       ];
       enabled-extensions = [
         "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
